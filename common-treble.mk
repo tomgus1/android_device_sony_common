4ec34432ecbed3b3c2c04580ee21d2c08d98cab9
Tim <tomgus1.2@gmail.com>
2019-01-04 19:13:20 +0100

    common: add android.hardware.radio.config
    Needed for RIL after LA.UM.7.3.r1-06600-sdm845.0 update

--- common-treble.mk
+++ common-treble.mk
@@ -24,7 +24,7 @@ PRODUCT_PACKAGES += \
     android.hardware.radio@1.2-radio-service \
     android.hardware.radio.config@1.0 \
     android.hardware.radio.deprecated@1.0 \
-	android.hardware.secure_element@1.0
+    android.hardware.secure_element@1.0
 
 # Audio
 PRODUCT_PACKAGES += \
